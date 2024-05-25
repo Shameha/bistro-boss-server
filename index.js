3ae01f7bef5dce5cd1faac930d1523daf51a5351
shamehaera9 <shamehaera9@gmail.com>
2024-05-26 01:53:34 +0600

    added new collection of database

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
 
     const menuCollection = client.db("bistrodb").collection("menu");
     const reviewCollection = client.db("bistrodb").collection("review");
+    const cartCollection = client.db("bistrodb").collection("carts");
 
   app.get('/menu',async(req,res)=>{
     const result = await menuCollection.find().toArray();
@@ -39,7 +40,18 @@ async function run() {
     res.send(result);
   })
 
+  //carts collection
+ app.get('/carts',async(req,res)=>{
+  const result = await cartCollection.find().toArray();
+  res.send(result);
+ })
 
+
+  app.post('/carts',async(req,res)=>{
+    const cartItem = req.body;
+    const result = await cartCollection.insertOne(cartItem);
+    res.send(result);
+  })
 
 
     // Send a ping to confirm a successful connection
