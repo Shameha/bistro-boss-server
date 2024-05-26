a2635de3a1739b418e75b677ef26ad8b2001da6f
shamehaera9 <shamehaera9@gmail.com>
2024-05-26 14:56:15 +0600

    added delete

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t86vw4m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,7 +42,9 @@ async function run() {
 
   //carts collection
  app.get('/carts',async(req,res)=>{
-  const result = await cartCollection.find().toArray();
+  const email = req.query.email;
+  const query = {email:email}
+  const result = await cartCollection.find(query).toArray();
   res.send(result);
  })
 
@@ -52,6 +54,14 @@ async function run() {
     const result = await cartCollection.insertOne(cartItem);
     res.send(result);
   })
+
+ app.delete('/carts/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query ={_id: new ObjectId(id) }
+  const result = await cartCollection.deleteOne(query);
+  res.send(result);
+ })
+
 
 
     // Send a ping to confirm a successful connection
