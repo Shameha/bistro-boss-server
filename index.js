2936dcb65776b6a56afb2a7bd60a64ed077cf46f
shamehaera9 <shamehaera9@gmail.com>
2024-05-29 23:12:13 +0600

    added user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 require(`dotenv`).config();
 //middleware
@@ -27,14 +28,89 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("bistrodb").collection("users");
     const menuCollection = client.db("bistrodb").collection("menu");
     const reviewCollection = client.db("bistrodb").collection("review");
     const cartCollection = client.db("bistrodb").collection("carts");
+  
+  
+    app.post('/jwt',async(req,res) =>{
+    const user = req.body;
+    const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+      expiresIn:'1h'});
+      res.send({token});
+  })
 
-  app.get('/menu',async(req,res)=>{
+   //middleware
+   const verifyToken =(req,res,next)=>{
+    console.log("inside verify",req.headers.authorization);
+    if(!req.headers.authorization){
+      return res.status(401).send({massage:'forbidden access'})
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+      if(err){
+        return res.status(401).send({message:'forbidden'})
+      }
+      req.decoded = decoded;
+      next();
+    })
+    // next();
+   }
+
+
+    //user related appi
+app.get('/users',verifyToken,async(req,res)=>{
+  // console.log("called");
+  const result = await userCollection.find().toArray();
+  res.send(result)
+})
+
+
+app.post('/users',async(req,res)=>{
+  const user = req.body;
+  //insert email user
+  const query ={email:user.email}
+  const existingUser = await userCollection.findOne(query);
+  if(existingUser){
+    return res.send({message:'user already exist1',insertedId : null})
+  }
+  const result = await userCollection.insertOne(user)
+  res.send(result)
+})
+
+app.patch('/users/admin/:id',async(req,res)=>{
+const id = req.params.id;
+const filter ={_id: new ObjectId(id)};
+const updateDoc ={
+
+  $set:{
+    role:'admin'
+  }
+
+}
+const result = await userCollection.updateOne(filter,updateDoc);
+res.send(result);
+
+
+})
+
+
+app.delete('/users/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query = {_id:new ObjectId(id)}
+  const result = await userCollection.deleteOne(query);
+  res.send(result)
+})
+
+
+
+
+app.get('/menu',async(req,res)=>{
     const result = await menuCollection.find().toArray();
     res.send(result);
   })
+
   app.get('/review',async(req,res)=>{
     const result = await reviewCollection.find().toArray();
     res.send(result);
